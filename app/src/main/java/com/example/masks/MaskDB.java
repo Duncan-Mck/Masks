7a97321d06de0bf82d31206362eeb58b5b5db094
Duncan-Mck <duncanmckirdy@gmail.com>
2019-09-20 18:46:59 -0400

    Changed constants

--- app/src/main/java/com/example/masks/MaskDB.java
+++ app/src/main/java/com/example/masks/MaskDB.java
@@ -12,65 +12,53 @@ public class MaskDB extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "MaskDB.db";
 
-    //Move Table
-    public static final String MOVE_TABLE_NAME = "moves";
-    public static final String MOVE_TABLE_ID = "move_id";
-    public static final String MOVE_TABLE_TITLE = "title";
-    public static final String MOVE_TABLE_MOVE_TYPE = "move_type"; //T - Team, G - Group, P - Playbook, B - Basic, A - Adult
-
-    //Playbook Move Table
-    public static final String PLAYBOOK_MOVE_TABLE_NAME = "playbook_moves";
-    public static final String PLAYBOOK_MOVE_TABLE_MOVE_ID = "move_id";
-    public static final String PLAYBOOK_MOVE_TABLE_PLAYBOOK_ID = "playbook_id";
-
-    //Move Text Table
-    public static final String MOVE_TEXT_TABLE_NAME = "move_texts";
-    public static final String MOVE_TEXT_MOVE_ID = "move_id";
-    public static final String MOVE_TEXT_ORDER_NUM = "order_num";
-    public static final String MOVE_TEXT_TEXT = "text";
-    public static final String MOVE_TEXT_TYPE = "type";
-
-    //Condition Table
-    public static final String CONDITION_TABLE_NAME = "conditions";
-    public static final String CONDITION_TABLE_ID = "condition_id";
-    public static final String CONDITION_TABLE_TITLE = "title";
-    public static final String CONDITION_TABLE_DESC = "desc";
-
-    //Condition Move Table
-    public static final String CONDITION_MOVE_TABLE_NAME = "condition_move";
-    public static final String CONDITION_MOVE_TABLE_MOVE_ID = "move_id";
-    public static final String CONDITION_MOVE_TABLE_CONDITION_ID = "condition_id";
-    public static final String CONDITION_MOVE_TABLE_MODIFIER = "move_modifier";
-
-    //Label Table
-    public static final String LABEL_TABLE_NAME = "labels";
-    public static final String LABEL_TABLE_ID = "label_id";
-    public static final String LABEL_TABLE_TITLE = "title";
-    public static final String LABEL_TABLE_DESC = "desc";
-
-    //Move Labels Table
-    public static final String MOVE_LABELS_TABLE_NAME = "move_labels";
-    public static final String MOVE_LABELS_TABLE_MOVE_ID = "move_id";
-    public static final String MOVE_LABELS_TABLE_LABEL_ID = "label_id";
-
-    //Playbook Table
-    public static final String PLAYBOOK_TABLE_NAME = "playbooks";
-    public static final String PLAYBOOK_TABLE_ID = "id";
-    public static final String PLAYBOOK_TABLE_TITLE = "title";
-    public static final String PLAYBOOK_TABLE_MOMENT_OF_TRUTH = "moment_of_truth";
-
-    //Advancement Table
-    public static final String ADVANCE_TABLE_NAME = "advancements";
-    public static final String ADVANCE_TABLE_ID = "id";
-    public static final String ADVANCE_TABLE_DESC = "desc";
-    public static final String ADVANCE_TABLE_LEVEL = "level"; //L - Lesser, G - Greater
-
-    //Playbook Advancement Table
-    public static final String PLAYBOOK_ADVANCE_TABLE_NAME = "playbook_advancements";
-    public static final String PLAYBOOK_ADVANCE_TABLE_PLAYBOOK_ID = "playbook_id";
-    public static final String PLAYBOOK_ADVANCE_TABLE_ADVANCE_ID = "advance_id";
-    public static final String PLAYBOOK_ADVANCE_TABLE_ADVANCE_NUM = "advance_num"; //usually 1, some playbooks have an advance more than 1 time, this counts up to them.
-
+    //Data Tables
+    public static final String TABLE_NAME_PLAYBOOK = "playbooks";
+    public static final String TABLE_NAME_MODULES = "modules;
+    public static final String TABLE_NAME_TEXTS = "texts";
+    public static final String TABLE_NAME_POWERS = "powers";
+    public static final String TABLE_NAME_MOVES = "moves";
+    public static final String TABLE_NAME_ADVANCEMENTS = "advancements";
+    public static final String TABLE_NAME_ABILITIES = "abilities";
+    public static final String TABLE_NAME_LABELS = "labels";
+    public static final String TABLE_NAME_CONDITIONS = "conditions";
+    public static final String TABLE_NAME_CHARACTER = "characters";
+    
+    //Relational Tables - Playbooks
+    public static final String TABLE_NAME_PLAYBOOK_POWERS = "playbookPowers";
+    public static final String TABLE_NAME_PLAYBOOK_ADVANCEMENTS = "playbookAdvancements";
+    public static final String TABLE_NAME_PLAYBOOK_ABILITIES = "playbookAbilities";
+    public static final String TABLE_NAME_PLAYBOOK_QUESTIONS = "playbookQuestions";
+    
+    //Relational Tables - Characters
+    public static final String TABLE_NAME_CHARACTER_CONDITIONS = "characterConditions";
+    public static final String TABLE_NAME_CHARACTER_DESCRIPTIONS = "characterDescriptions";
+    public static final String TABLE_NAME_CHARACTER_MODS = "characterMods";
+    public static final String TABLE_NAME_CHARACTER_LABELS = "characteLabels";
+    public static final String TABLE_NAME_CHARACTER_ADVANCEMENTS = "characterAdvancements";
+    public static final String TABLE_NAME_CHARACTER_ABILITIES = "characterAbilities"
+    
+    //Relational Tables - Powers/Moves
+    public static final String TABLE_NAME_POWER_TEXTS = "powerTexts";
+    public static final String TABLE_NAME_MOVE_TEXTS = "moveTexts";
+    public static final String TABLE_NAME_POWER_MOVES = "powerMoves";
+    public static final String TABLE_NAME_POWER_LABELS = "powerLabels";
+    public static final String TABLE_NAME_POWER_HOLDS = "powerHolds";
+    public static final String TABLE_NAME_POWER_MODS = "powerMods";
+    public static final String TABLE_NAME_MOVE_MODS = "moveMods";
+    
+    //Custom Columns    
+    public static final String COLUMN_NAME_MAX_NUM_ABILITIES = "maxNumAbilities";
+    public static final String COLUMN_NAME_MIN_NUM_ABILITIES = "minNumAbilities";
+    public static final String COLUMN_NAME_NUM_STARTING_MOVES = "numStartingMoves";
+        
+    //Common Columns
+    public static final String COLUMN_NAME_DESC = "desc";
+    public static final String COLUMN_NAME_ORDER = "order";
+    public static final String COLUMN_NAME_NAME = "name";
+    public static final String COLUMN_NAME_VALUE = "value";
+    public static final String COLUMN_NAME_TYPE = "type";
+    
     public MaskDB(Context context) {
         super(context, DATABASE_NAME , null, 1);
     }
